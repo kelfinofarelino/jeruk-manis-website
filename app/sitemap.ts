3b75329ab930adf2100ef749467b523a557dc96a
kelfinofarelino <farelinokelvin@gmail.com>
2026-01-07 21:40:32 +0700

    add sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://jerukmanis.web.id' // Ganti domain aslimu
+  const baseUrl = 'https://jerukmanis.web.id'
 
   return [
     {
