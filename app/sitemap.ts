9675db6a986a8260e1551fda14592050c2d4e555
kelfinofarelino <farelinokelvin@gmail.com>
2026-01-07 21:38:58 +0700

    add sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,32 @@
 import { MetadataRoute } from 'next'
-
+ 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://jerukmanis.web.id' // Ganti domain aslimu
+
   return [
     {
-      url: 'https://jerukmanis.web.id',
+      url: baseUrl,
       lastModified: new Date(),
-    }
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/layanan/fotografer-jogja`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/layanan/videografer-jogja`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/tools/qr-generator`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
   ]
 }
