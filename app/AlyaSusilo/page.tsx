333954e61a3b77406d62a66393e86d6dcfe7f455
kelfinofarelino <farelinokelvin@gmail.com>
2026-08-11 15:20:02 +0700

    bikin shorten link

---
+++ app/AlyaSusilo/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export default function AlyaSusiloPage() {
+  // Ganti URL di dalam tanda kurung ini dengan link tujuan (forward) yang kamu inginkan
+  redirect("https://instagram.com/alyasusilo"); 
+}
